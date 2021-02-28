5c4f261cbf16d3b3f767fa53185838aec429b7b3
Takashi IIGUNI <iiguni.tks@gmail.com>
2021-02-28 06:55:50 +0000

    move to tiny_tracer to main bin program

--- sprofiler-bpf/src/main.rs
+++ sprofiler-bpf/src/main.rs
@@ -1,6 +1,6 @@
 use std::collections::HashSet;
-use std::fs;
-use std::io;
+use std::fs::{self, File};
+use std::io::prelude::*;
 use std::os::unix::fs::MetadataExt;
 use std::path::PathBuf;
 use std::str;
@@ -9,9 +9,12 @@ use std::sync::{Arc, Mutex};
 
 use lazy_static::lazy_static;
 use libbpf_rs::PerfBufferBuilder;
+use nix::unistd::daemon;
 use plain::Plain;
+use structopt::StructOpt;
 
 use sprofiler_bpf::*;
+use sprofiler_sys::oci::{Arch, LinuxSeccomp, LinuxSeccompAction, LinuxSyscall};
 
 #[repr(C)]
 #[derive(Default, Debug)]
@@ -23,6 +26,22 @@ struct SysEnterEvent {
 }
 
 unsafe impl Plain for SysEnterEvent {}
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "sprofiler-bpf", about = "Dynamic seccomp profiler with eBPF")]
+struct Command {
+    /// Filter container from container id
+    #[structopt(short = "c", long = "container-id")]
+    container_id: String,
+
+    /// Output file
+    #[structopt(short = "o", long = "out")]
+    out: PathBuf,
+
+    /// Pidfile
+    #[structopt(short = "p", long = "pid-file")]
+    pid_file: PathBuf,
+}
 
 fn handle_event(_cpu: i32, data: &[u8]) {
     let mut event = SysEnterEvent::default();
@@ -39,7 +58,7 @@ fn handle_event(_cpu: i32, data: &[u8]) {
         syscall_list.insert(syscall_name);
     }
 
-    // println!("syscall: {}", syscall_name);
+    println!("syscall: {}", syscall_name);
 }
 
 fn handle_lost_event(cpu: i32, count: u64) {
@@ -50,12 +69,28 @@ lazy_static! {
     static ref SYSCALL_LIST: Mutex<HashSet<&'static str>> = Mutex::new(HashSet::new());
 }
 
-fn show_syscalls() {
+fn output_syscalls(path: PathBuf) -> anyhow::Result<()> {
+    let file = File::create(path)?;
     let syscall_list = SYSCALL_LIST.lock().unwrap();
-    let mut syscall_list: Vec<&'static str> = syscall_list.clone().into_iter().collect();
+    let mut syscall_list: Vec<String> = syscall_list
+        .clone()
+        .into_iter()
+        .map(|s| s.to_string())
+        .collect();
     syscall_list.sort();
-    let s = syscall_list.join(", ");
-    println!("{}", s);
+
+    let seccomp_profile = LinuxSeccomp {
+        syscalls: Some(vec![LinuxSyscall {
+            names: syscall_list,
+            action: LinuxSeccompAction::SCMP_ACT_ALLOW,
+            args: None,
+        }]),
+        default_action: LinuxSeccompAction::SCMP_ACT_ERRNO,
+        architectures: Some(vec![Arch::SCMP_ARCH_X86_64]),
+    };
+
+    serde_json::to_writer(&file, &seccomp_profile)?;
+    Ok(())
 }
 
 /// only work when cgroup driver is systemd & v2
@@ -66,11 +101,21 @@ fn get_contianer_cgroup_id(container_id: &str) -> anyhow::Result<u64> {
     Ok(meta.ino())
 }
 
+fn create_pid_file(path: PathBuf, pid: u32) -> anyhow::Result<()> {
+    let mut file = File::create(path)?;
+    file.write_all(&pid.to_string().as_bytes())?;
+
+    Ok(())
+}
+
 fn main() -> anyhow::Result<()> {
+    let command = Command::from_args();
+    create_pid_file(command.pid_file, std::process::id())?;
+
     let mut skel_builder = SystraceSkelBuilder::default();
     let mut systrace_skel = skel_builder.open()?;
 
-    systrace_skel.rodata().target_cgid = get_contianer_cgroup_id("container-id")?;
+    systrace_skel.rodata().target_cgid = get_contianer_cgroup_id(&command.container_id)?;
 
     let mut skel = systrace_skel.load()?;
 
@@ -89,8 +134,7 @@ fn main() -> anyhow::Result<()> {
         .lost_cb(handle_lost_event)
         .build()?;
 
-    println!("PID: {}", std::process::id());
-    println!("Press Ctrl-C to exit");
+    daemon(true, false)?;
 
     while running.load(Ordering::SeqCst) {
         if let Err(err) = perf.poll(std::time::Duration::from_millis(100)) {
@@ -99,7 +143,7 @@ fn main() -> anyhow::Result<()> {
         }
     }
 
-    show_syscalls();
+    output_syscalls(command.out)?;
 
     Ok(())
 }
