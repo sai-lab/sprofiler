71ef793f4ecf14d6340fde2f5ab0e5377d550f0d
Takashi IIGUNI <iiguni.tks@gmail.com>
2021-04-15 02:10:06 +0000

    debug-container: add run-with-seccomp function

--- sprofiler-bpf/scripts/debug-container.sh
+++ sprofiler-bpf/scripts/debug-container.sh
@@ -2,34 +2,62 @@
 
 set -eux
 
+PROFILE=/tmp/seccomp-profile.json
+TRACE_CONTAINER_ID_FILE=/tmp/nginx.cid
+CONTAIN_SCMP_CONTAINER_ID_FILE=/tmp/nginx-with-seccomp.cid
+
 function run() {
     podman \
         --hooks-dir $(pwd)/hooks \
-        run -d --cidfile=/tmp/debug-container.cid \
+        run -d \
+        --cidfile $TRACE_CONTAINER_ID_FILE \
         -p 8081:80 \
-        --annotation "io.sprofiler.output_seccomp_profile_path=/tmp/seccomp-profile.json" \
-        docker.io/library/nginx
+        --annotation "io.sprofiler.output_seccomp_profile_path=${PROFILE}" \
+        docker.io/library/nginx:1.19
+}
+
+function enter() {
+    podman \
+        exec -it $(cat $TRACE_CONTAINER_ID_FILE) /bin/bash
 }
 
 function stop() {
     podman \
         --hooks-dir $(pwd)/hooks \
-        stop $(cat /tmp/debug-container.cid)
-    rm /tmp/debug-container.cid
+        stop $(cat $TRACE_CONTAINER_ID_FILE)
+
+    rm $TRACE_CONTAINER_ID_FILE
+    cat $PROFILE | jq .
 }
 
 function state() {
-    crun --root /run/crun state $(cat /tmp/debug-container.cid)
+    crun --root /run/crun state $(cat $TRACE_CONTAINER_ID_FILE)
 }
 
 function trace-stop() {
-    bundle=$(crun --root /run/crun state $(cat /tmp/debug-container.cid) | jq .bundle | sed -e 's/^"//' -e 's/"$//' )
+    bundle=$(crun --root /run/crun state $(cat $TRACE_CONTAINER_ID_FILE) | jq .bundle | sed -e 's/^"//' -e 's/"$//' )
     # pkill -SIGUSR1 $(cat $bundle/sprofiler.pid)
     pkill -SIGTERM $(cat $bundle/sprofiler.pid)
 }
 
 function trace-kill-all() {
     /bin/kill -SIGKILL $(pidof sprofiler-bpf)
+}
+
+function run-with-seccomp() {
+    podman \
+        run -d  \
+        --cidfile=$CONTAIN_SCMP_CONTAINER_ID_FILE \
+        -p 8080:80 \
+        --security-opt seccomp=/tmp/seccomp-profile.json \
+        docker.io/library/nginx:1.19
+}
+
+function stop-seccomp-container() {
+    podman \
+        stop $(cat $CONTAIN_SCMP_CONTAINER_ID_FILE)
+
+    rm $CONTAIN_SCMP_CONTAINER_ID_FILE
 }
 
 $1
