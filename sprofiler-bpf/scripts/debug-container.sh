68ada77079a4e25049fa8650d647d2a5a6aec428
Takashi IIGUNI <iiguni.tks@gmail.com>
2021-04-20 09:51:27 +0000

    [sprofiler-bpf] fix debug script

--- sprofiler-bpf/scripts/debug-container.sh
+++ sprofiler-bpf/scripts/debug-container.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-set -eux
+set -u
 
 PROFILE=/tmp/seccomp-profile.json
 TRACE_CONTAINER_ID_FILE=/tmp/nginx.cid
 CONTAIN_SCMP_CONTAINER_ID_FILE=/tmp/nginx-with-seccomp.cid
 IMAGE=docker.io/library/nginx:1.19
+# IMAGE=docker.io/guni1192/sprofiler-demo-app
 
 function run() {
     if [ -f $PROFILE ]; then
-        rm $PROFILE
+        rm -rf $PROFILE
     fi
 
     podman \
         --hooks-dir $(pwd)/hooks \
         run -d \
+        --rm \
         --cidfile $TRACE_CONTAINER_ID_FILE \
         -p 8081:80 \
         --annotation "io.sprofiler.output_seccomp_profile_path=${PROFILE}" \
@@ -31,7 +33,7 @@ function stop() {
         --hooks-dir $(pwd)/hooks \
         stop $(cat $TRACE_CONTAINER_ID_FILE)
 
-    rm $TRACE_CONTAINER_ID_FILE
+    rm -rf $TRACE_CONTAINER_ID_FILE
 }
 
 function state() {
@@ -50,6 +52,7 @@ function trace-kill-all() {
 function run-with-seccomp() {
     podman \
         run -d  \
+        --rm \
         --cidfile=$CONTAIN_SCMP_CONTAINER_ID_FILE \
         -p 8080:80 \
         --security-opt seccomp=/tmp/seccomp-profile.json \
@@ -60,7 +63,7 @@ function stop-seccomp-container() {
     podman \
         stop $(cat $CONTAIN_SCMP_CONTAINER_ID_FILE)
 
-    rm $CONTAIN_SCMP_CONTAINER_ID_FILE
+    rm -rf $CONTAIN_SCMP_CONTAINER_ID_FILE
 }
 
 $1
