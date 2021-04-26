ccd8d8e085622a5db744cb6ad6f3c48bd08584c5
Takashi IIGUNI <iiguni.tks@gmail.com>
2021-04-26 04:57:52 +0000

    [sprofiler] add subcommand

--- sprofiler/src/main.rs
+++ sprofiler/src/main.rs
@@ -6,33 +6,46 @@ use sprofiler_sys::lang::SeccompProfiler;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "sprofile")]
-struct Command {
-    /// Activate debug mode
-    #[structopt(short, long)]
-    debug: bool,
+#[structopt(name = "sprofiler")]
+enum Command {
+    Run {
+        /// Input binary file
+        #[structopt(short, long, parse(from_os_str))]
+        bin: PathBuf,
 
-    /// Input binary file
-    #[structopt(short, long, parse(from_os_str))]
-    bin: PathBuf,
+        /// Output seccomp profile path
+        #[structopt(short, long, parse(from_os_str))]
+        out: PathBuf,
+        // TODO: Language Option
+        // #[structopt(short, long)]
+        // lang: String
+    },
+    Compare {},
+    Merge {
+        #[structopt(parse(from_os_str))]
+        paths: Vec<PathBuf>,
+    },
+}
 
-    /// Output seccomp profile path
-    #[structopt(short, long, parse(from_os_str))]
-    out: PathBuf,
-    // TODO: Language Option
-    // #[structopt(short, long)]
-    // lang: String
+fn do_run(bin: PathBuf, out: PathBuf) -> Result<()> {
+    let sprofiler = GoSeccompProfiler {
+        target_bin: bin,
+        destination: out,
+    };
+
+    sprofiler.output()?;
+
+    Ok(())
 }
 
 fn main() -> Result<()> {
     let command = Command::from_args();
 
-    let sprofiler = GoSeccompProfiler {
-        target_bin: command.bin,
-        destination: command.out,
+    match command {
+        Command::Run { bin, out } => do_run(bin, out)?,
+        Command::Compare { .. } => {}
+        Command::Merge { .. } => {}
     };
-
-    sprofiler.output()?;
 
     Ok(())
 }
